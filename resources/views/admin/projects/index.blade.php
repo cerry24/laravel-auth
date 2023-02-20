cf660c45d1a7d74e99bd4737e04b99e44da30425
cerry24 <samuelecerretti@gmail.com>
2023-02-20 18:21:14 +0100

    implement admin layout + index function & dispaly

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">id</th>
+                            <th scope="col">title</th>
+                            <th scope="col">creation date</th>
+                            <th scope="col"><a class="btn btn-success" href="">add new project</a></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($projects as $project)
+                        <tr>
+                            <th scope="row">{{ $project->id }}</th>
+                            <td>{{ $project->title }}</td>
+                            <td>{{ $project->creation_date }}</td>
+                            <td>
+                                <a class="btn btn-primary" href="">show</a>
+                                <a class="btn btn-warning" href="">edit</a>
+                                <a class="btn btn-danger" href="">delete</a>
+                            </td>
+                        </tr>
+                        @empty
+                            <p>There are no projects to be shown</p>
+                        @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController as DashboardController;
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +28,7 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
         Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('/projects', AdminProjectController::class);
     });
 
 require __DIR__.'/auth.php';
