2faa68fe547260b0ecd78aa42a2f8f3b8ed12567
cerry24 <samuelecerretti@gmail.com>
2023-02-21 18:19:56 +0100

    quick edits on admin layout & dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -36,6 +36,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.projects.index') }}">{{ __('Projects') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -74,7 +77,7 @@
             </div>
         </nav>
 
-        <main class="">
+        <main class="mt-4">
             @yield('content')
         </main>
     </div>
