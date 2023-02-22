3a112aff34dfa470082f9d851087a39bf02528ad
cerry24 <samuelecerretti@gmail.com>
2023-02-22 15:57:50 +0100

    add dropdown item to go to projects + link to view the preview of guest home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
                             <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{route('admin.projects.index') }}">{{ __('projects') }}</a>
+                            <a class="nav-link" href="{{route('admin.projects.index') }}">{{ __('Projects') }}</a>
                         </li>
                     </ul>
 
