06ffa272329291a63b33d3c5eb713e1e8ed355d4
cerry24 <samuelecerretti@gmail.com>
2023-02-24 15:35:34 +0100

    edit create/edit form for uploading a file as thumbnail

--- resources/views/admin/projects/partials/createEditForm.blade.php
+++ resources/views/admin/projects/partials/createEditForm.blade.php
@@ -1,4 +1,4 @@
-<form action="{{ route($routeName, $project) }}" method="POST">
+<form action="{{ route($routeName, $project) }}" method="POST" enctype="multipart/form-data">
     @csrf
     @method($method)
 
@@ -12,7 +12,7 @@
     
     <div class="mb-3">
         <label for="input-thumbnail" class="form-label">Thumbnail</label>
-        <input type="text" class="form-control @error('thumbnail') is-invalid @enderror" name="thumbnail" value="{{ old('title', $project->thumbnail) }}" id="input-thumbnail">
+        <input type="file" class="form-control @error('thumbnail') is-invalid @enderror" name="thumbnail" value="{{ old('thumbnail', $project->thumbnail) }}" id="input-thumbnail">
         @error('thumbnail')
             <div class="invalid-feedback">{{ $message }}</div>
         @enderror
@@ -20,15 +20,15 @@
 
     <div class="mb-3">
         <label for="input-description" class="form-label">Description</label>
-        <textarea name="description" class="form-control @error('description') is-invalid @enderror" id="input-description" cols="30" rows="10">{{ old('title', $project->description) }}</textarea>
+        <textarea name="description" class="form-control @error('description') is-invalid @enderror" id="input-description" cols="30" rows="10">{{ old('description', $project->description) }}</textarea>
         @error('description')
             <div class="invalid-feedback">{{ $message }}</div>
         @enderror
     </div>
 
     <div class="mb-3">
-        <label for="input-price" class="form-label">Creation date</label>
-        <input type="datetime" class="form-control @error('creation_date') is-invalid @enderror" name="creation_date" value="{{ old('title', $project->creation_date) }}" id="input-creation_date">
+        <label for="creation_date" class="form-label">Creation date</label>
+        <input type="datetime" class="form-control @error('creation_date') is-invalid @enderror" name="creation_date" value="{{ old('creation_date', $project->creation_date) }}" id="input-creation_date">
         @error('creation_date')
             <div class="invalid-feedback">{{ $message }}</div>
         @enderror
